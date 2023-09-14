c5f7f84cd7253f123fea9974d5b8efa62dc10bd3
MarkOmelyanenko <famil.mark@gmail.com>
2023-09-14 15:15:07 +0200

    lambda updated

--- src/com/company/Lambda/Lambda.java
+++ src/com/company/Lambda/Lambda.java
@@ -1,4 +1,17 @@
 package com.company.Lambda;
 
 public class Lambda {
+    public static void main(String[] args) {
+        Operationable operation1 = (x, y) -> x + y;
+        Operationable operation2 = (int x, int y) -> {
+            if (y > 10) return x / y;
+            else return x * y;
+        };
+
+        System.out.println(operation1.calculate(3, 12));
+        System.out.println(operation2.calculate(100, 9));
+
+        Printable printer = s -> System.out.println(s);
+        printer.print("Hello");
+    }
 }
---
+++ src/com/company/Lambda/Printable.java
@@ -0,0 +1,5 @@
+package com.company.Lambda;
+
+public interface Printable {
+    void print(String s);
+}
